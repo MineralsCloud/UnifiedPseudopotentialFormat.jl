72307af526f4f5274f8014ac3914bf65461a7c6b
Documenter.jl <documenter@juliadocs.github.io>
2021-12-26 01:19:51 +0000

    build based on b10ea59

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = UnifiedPseudopotentialFormat","category":"page"},{"location":"#UnifiedPseudopotentialFormat","page":"Home","title":"UnifiedPseudopotentialFormat","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UnifiedPseudopotentialFormat.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [UnifiedPseudopotentialFormat]","category":"page"}]
+}
